55526bd3ec781fd571d8a159cee68b4009c34df4
Swapnil_Alase <swapnilalase07@gmail.com>
2020-09-14 12:15:11 +0530

    Update project1.groovy

--- jobs/project1.groovy
+++ jobs/project1.groovy
@@ -1,1 +1,17 @@
-s
+
+pipelineJob('Project1') {
+
+  def repo = 'https://github.com/SwapnilAlase25/project1.git'
+  
+  description("Project1_pipeline_job")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+        }
+      }
+    }
+  }
+}
